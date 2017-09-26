4945559abb26e1cb8acdd8e0a9bd0af1d7651641
rubenr <rubenr@dropbox.com>
2017-09-26 08:56:39 -0700

    backend

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
 	DBX_APP_SECRET:'<appsecret_in_dropbox_console>',
 	OAUTH_REDIRECT_URL:"http://localhost:3000/oauthredirect",
 	DBX_LIST_FOLDER_PATH:'/2/files/list_folder',
-    DBX_LIST_FOLDER_CONTINUE_PATH:'/2/files/list_folder/continue',
-    DBX_GET_TEMPORARY_LINK_PATH:'/2/files/get_temporary_link',
+	DBX_LIST_FOLDER_CONTINUE_PATH:'/2/files/list_folder/continue',
+	DBX_GET_TEMPORARY_LINK_PATH:'/2/files/get_temporary_link',
 }
