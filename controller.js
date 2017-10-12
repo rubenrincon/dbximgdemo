e309940b9562cd5202f59748fb33be62273d3b82
rubenr <rubenr@dropbox.com>
2017-10-12 11:15:43 -0700

    fixed formatting

--- controller.js
+++ controller.js
@@ -1,58 +1,50 @@
 
-
 const 
 crypto = require('crypto'),
 config = require('./config'),
-NodeCache = require( "node-cache" );
-var mycache = new NodeCache();
+NodeCache = require( "node-cache" ),
+mycache = new NodeCache();
 rp = require('request-promise');
 
-//steps 1,2,3
+
 module.exports.home = async (req,res,next)=>{    
-    
-    //let token = mycache.get("aTempTokenKey");
-    let token = req.session.token;
-    if(token){
-      
-      try{
 
-        let paths = await getLinksAsync(token); 
+  let token = req.session.token;
+  if(token){
+    try{
 
-        if(paths.length>0){
-          res.render('gallery', { imgs: paths, layout:false});
+      let paths = await getLinksAsync(token); 
 
-        }else{
-          res.render('empty', {layout:false});
-        }
-        
+      if(paths.length>0){
+        res.render('gallery', { imgs: paths, layout:false});
+      }else{
+        //if no images, ask user to upload some
+        res.render('empty', {layout:false});
+      }    
 
-      }catch(error){
-        return next(new Error("Error getting images from Dropbox"));
-      }
-    
-    }else{
-        res.redirect('/login');
+    }catch(error){
+      return next(new Error("Error getting images from Dropbox"));
     }
+
+  }else{
+    res.redirect('/login');
+  }
 };
 
-//steps 4,5,6
-module.exports.login = (req,res,next)=>{
-    
-    //create a random state value
-    let state = crypto.randomBytes(16).toString('hex');
-     
-    //Save state and temporarysession for 10 mins
 
-    // mycache.set(state, "aTempSessionValue", 600);
-    mycache.set(state, req.sessionID, 600);
-     
-    let dbxRedirect= config.DBX_OAUTH_DOMAIN 
-            + config.DBX_OAUTH_PATH 
-            + "?response_type=code&client_id="+config.DBX_APP_KEY
-            + "&redirect_uri="+config.OAUTH_REDIRECT_URL 
-            + "&state="+state;
-    
-    res.redirect(dbxRedirect);
+module.exports.login = (req,res,next)=>{
+
+  //create a random state value and store it for 10 mins
+  let state = crypto.randomBytes(16).toString('hex');
+  mycache.set(state, req.sessionID, 600);
+
+  let dbxRedirect= config.DBX_OAUTH_DOMAIN 
+  + config.DBX_OAUTH_PATH 
+  + "?response_type=code&client_id="+config.DBX_APP_KEY
+  + "&redirect_uri="+config.OAUTH_REDIRECT_URL 
+  + "&state="+state;
+  
+  res.redirect(dbxRedirect);
 }
 
 
@@ -68,60 +60,57 @@ module.exports.logout = async (req,res,next)=>{
 }
 
 
-//steps 8-12
 module.exports.oauthredirect = async (req,res,next)=>{
 
-      if(req.query.error_description){
-          return next( new Error(req.query.error_description));
-      } 
+  if(req.query.error_description){
+    return next( new Error(req.query.error_description));
+  } 
 
-      let state= req.query.state;
+  let state= req.query.state;
 
-      //if(!mycache.get(state)){
-      if(mycache.get(state)!=req.sessionID){
-          return next(new Error("session expired or invalid state"));
-      } 
+  //compare state in the query with previously stored state
+  if(mycache.get(state)!=req.sessionID){
+    return next(new Error("session expired or invalid state"));
+  } 
 
-      //Exchange code for token
-      if(req.query.code ){
-  
-          let options={
-              url: config.DBX_API_DOMAIN + config.DBX_TOKEN_PATH, 
-              //build query string
-              qs: {'code': req.query.code, 
-                  'grant_type': 'authorization_code', 
-                  'client_id': config.DBX_APP_KEY, 
-                  'client_secret':config.DBX_APP_SECRET,
-                  'redirect_uri':config.OAUTH_REDIRECT_URL}, 
-              method: 'POST',
-              json: true }
+  //Exchange code for token
+  if(req.query.code ){
 
-          try{
+    let options={
+    url: config.DBX_API_DOMAIN + config.DBX_TOKEN_PATH, 
+        //build query string
+        qs: {'code': req.query.code, 
+        'grant_type': 'authorization_code', 
+        'client_id': config.DBX_APP_KEY, 
+        'client_secret':config.DBX_APP_SECRET,
+        'redirect_uri':config.OAUTH_REDIRECT_URL}, 
+        method: 'POST',
+        json: true }
 
-              let response = await rp(options);
-              
+    try{
 
-              //mycache.set(tempsession, response.access_token, 3600);
-              await regenerateSessionAsync(req);
-              req.session.token = response.access_token;
-              
-              res.redirect("/");
+      let response = await rp(options);
+      
+      await regenerateSessionAsync(req);
+      req.session.token = response.access_token;
+      
+      res.redirect("/");
 
-          }catch(error){
-                  return next(new Error('error getting token. '+error.message));
-          }        
-      }
+    }catch(error){
+      return next(new Error('error getting token. '+error.message));
+    }        
+  }
 }
 
 
 
 //Returns a promise that fulfills when a new session is created
-var regenerateSessionAsync = (req)=>{
-    return new Promise((resolve,reject)=>{
-            req.session.regenerate((err)=>{
-                err ? reject(err) : resolve();
-        });
+async function regenerateSessionAsync(req){
+  return new Promise((resolve,reject)=>{
+    req.session.regenerate((err)=>{
+      err ? reject(err) : resolve();
     });
+  });
 }
 
 
@@ -129,7 +118,7 @@ var regenerateSessionAsync = (req)=>{
 It is a two step process:
 1.  Get a list of all the paths of files in the folder
 2.  Fetch a temporary link for each file in the folder */
-var getLinksAsync = async function (token){
+async function getLinksAsync(token){
 
   //List images from the root of the app folder
   let result= await listImagePathsAsync(token,'');
@@ -149,60 +138,60 @@ var getLinksAsync = async function (token){
 /*
 Returns an object containing an array with the path_lower of each 
 image file and if more files a cursor to continue */
-var listImagePathsAsync = async (token,path) =>{
+async function listImagePathsAsync(token,path){
 
   let options={
-          url: config.DBX_API_DOMAIN + config.DBX_LIST_FOLDER_PATH, 
-          headers:{"Authorization":"Bearer "+token},
-          method: 'POST',
-          json: true ,
-          body: {"path":path}
-      }
+    url: config.DBX_API_DOMAIN + config.DBX_LIST_FOLDER_PATH, 
+    headers:{"Authorization":"Bearer "+token},
+    method: 'POST',
+    json: true ,
+    body: {"path":path}
+  }
 
   try{
-          //Make request to Dropbox to get list of files
-          let result = await rp(options);
-  
-          //Filter response to images only
-          let entriesFiltered= result.entries.filter(function(entry){
-                  return entry.path_lower.search(/\.(gif|jpg|jpeg|tiff|png)$/i) > -1;
-          });        
-  
-          //Get an array from the entries with only the path_lower fields
-          var paths = entriesFiltered.map(function (entry) {
-            return entry.path_lower;
-          });
-  
-          //return a cursor only if there are more files in the current folder
-          let response= {};
-          response.paths= paths;
-          if(result.hasmore) response.cursor= result.cursor;        
-          return response;
+    //Make request to Dropbox to get list of files
+    let result = await rp(options);
+
+    //Filter response to images only
+    let entriesFiltered= result.entries.filter(function(entry){
+      return entry.path_lower.search(/\.(gif|jpg|jpeg|tiff|png)$/i) > -1;
+    });        
+
+    //Get an array from the entries with only the path_lower fields
+    var paths = entriesFiltered.map(function (entry) {
+      return entry.path_lower;
+    });
+
+    //return a cursor only if there are more files in the current folder
+    let response= {};
+    response.paths= paths;
+    if(result.hasmore) response.cursor= result.cursor;        
+    return response;
 
   }catch(error){
-          return next(new Error('error listing folder. '+error.message));
+    return next(new Error('error listing folder. '+error.message));
   }        
 } 
 
 
 //Returns an array with temporary links from an array with file paths
-var getTemporaryLinksForPathsAsync = async (token,paths)=>{
+async function getTemporaryLinksForPathsAsync(token,paths){
 
-      var promises = [];
-      let options={
-          url: config.DBX_API_DOMAIN + config.DBX_GET_TEMPORARY_LINK_PATH, 
-          headers:{"Authorization":"Bearer "+token},
-          method: 'POST',
-          json: true
-      }
+  var promises = [];
+  let options={
+    url: config.DBX_API_DOMAIN + config.DBX_GET_TEMPORARY_LINK_PATH, 
+    headers:{"Authorization":"Bearer "+token},
+    method: 'POST',
+    json: true
+  }
   
-      //Create a promise for each path and push it to an array of promises
-      paths.forEach((path_lower)=>{
-              options.body = {"path":path_lower};
-          promises.push(rp(options));
-      });
-  
-      //returns a promise that fullfills once all the promises in the array complete or one fails
-      return Promise.all(promises);
+  //Create a promise for each path and push it to an array of promises
+  paths.forEach((path_lower)=>{
+    options.body = {"path":path_lower};
+    promises.push(rp(options));
+  });
+
+  //returns a promise that fullfills once all the promises in the array complete or one fails
+  return Promise.all(promises);
 
 }
